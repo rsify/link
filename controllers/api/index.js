017c18d35603f41a010bb21d79f8d93964acddf3
nikersify <nikersifyyt@gmail.com>
2017-02-27 22:37:02 +0100

    begin api routes

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -1,3 +1,13 @@
 const router = require('express').Router()
+const bodyParser = require('body-parser')
+
+router.use(bodyParser.urlencoded({
+	extended: true
+}))
+
+router.get('/has', require('./has'))
+router.post('/new', require('./new'))
+router.get('/resolve', require('./resolve'))
+router.get('/stats', require('./stats'))
 
 module.exports = router
