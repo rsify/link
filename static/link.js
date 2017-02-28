9319d74bf89a70b3c9a4d285fedf5cdb0262b05e
nikersify <nikersifyyt@gmail.com>
2017-02-28 20:50:22 +0100

    front-end shenanigans

--- static/link.js
+++ static/link.js
@@ -1,1 +1,15 @@
-console.log('link.js')
+var arr = ['start', 'loading', 'finish']
+
+for (let c of arr) {
+	Vue.component(c + '-component', {
+		template: '#' + c + '-template'
+	})
+}
+
+var vm = new Vue({
+	el: '#app',
+	template: '#app-template',
+	data: {
+		currentView: 'start-component'
+	}
+})
