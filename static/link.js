fd831a238285795175e286cea57ab89db33854de
nikersify <nikersifyyt@gmail.com>
2017-03-02 18:07:55 +0100

    vue wiring

--- static/link.js
+++ static/link.js
@@ -1,7 +1,7 @@
 var arr = ['start', 'loading', 'finish']
 
 for (let c of arr) {
-	Vue.component(c + '-component', {
+	Vue.component(c + '-view', {
 		template: '#' + c + '-template'
 	})
 }
@@ -9,7 +9,58 @@ for (let c of arr) {
 var vm = new Vue({
 	el: '#app',
 	template: '#app-template',
+	computed: {
+		linkOutputValue: function () {
+			if (!this.lOutputValue) return ''
+			else return window.location.origin + '/' + this.lOutputValue
+		}
+	},
 	data: {
-		currentView: 'start-component'
+		currentView: 'start-view',
+		lOutputValue: '',
+		urlInputValue: ''
+	},
+	methods: {
+		copylOutput: function () {
+			// TODO
+		},
+
+		finished: function () {
+			this.currentView = 'finish-view'
+		},
+
+		submit: function () {
+			if (this.urlInputValue.length === 0) return
+			this.currentView = 'loading-view'
+
+			var xhr = new XMLHttpRequest()
+			xhr.open('POST', '/api/new')
+			xhr.setRequestHeader('Content-type',
+				'application/x-www-form-urlencoded')
+
+			xhr.send('url=' + this.urlInputValue)
+
+			var that = this
+			xhr.onreadystatechange = function () {
+				if (xhr.readyState === XMLHttpRequest.DONE) {
+					try {
+						var body = JSON.parse(xhr.responseText)
+
+						that.lOutputValue = body.res.l
+						if (!that.lOutputValue) throw 'l not received'
+
+						that.finished()
+					} catch (e) {
+						console.log(e)
+					}
+				}
+			}
+		}
+	},
+	watch: {
+		urlInputValue: function (val, old) {
+			if (val.length > old.length + 1)
+				this.submit()
+		}
 	}
 })
